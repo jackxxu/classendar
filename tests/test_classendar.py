e8ca973ba13f6310b67991a571741cd5c0cfae6e
Jack Xiaosong Xu <jackxxu@users.noreply.github.com>
2025-07-26 00:28:19 +0000

    fix bug

--- tests/test_classendar.py
+++ tests/test_classendar.py
@@ -18,12 +18,41 @@ class Foo_20250701(Foo):
         return "Foo_20250701 result"
 
 
+class Foo_20250801(Foo_20250701):
+    async def get(self, **kwargs):
+        return "Foo_20250801 result"
+
+
+class Foo_20250901(Foo_20250801):
+    async def get(self, **kwargs):
+        result = await super().get(**kwargs)
+        return f"Foo_20250801 result + {result}"
+
+
 @pytest.mark.asyncio
-async def test_classender():
-    # Test
+async def test_base_class():
     foo = Foo(foo_arg="test")
-    result = await foo.get(date=date(2025, 7, 15))  # Returns "Foo_20250701 result"
+    result = await foo.get(date=date(2025, 6, 15))  
+    assert result == 'Base Foo result'    
+
+
+@pytest.mark.asyncio
+async def test_2nd_layer():
+    foo = Foo(foo_arg="test")
+    result = await foo.get(date=date(2025, 7, 15))  
     assert result == 'Foo_20250701 result'
 
-    result = await foo.get(date=date(2025, 6, 15))  # Returns "Foo_20250701 result"
-    assert result == 'Base Foo result'    
+
+@pytest.mark.asyncio
+async def test_3rd_layer():
+    foo = Foo(foo_arg="test")
+    result = await foo.get(date=date(2025, 8, 1))  
+    assert result == 'Foo_20250801 result'
+
+
+@pytest.mark.asyncio
+async def test_reference_base():
+    foo = Foo(foo_arg="test")
+    result = await foo.get(date=date(2025, 9, 1))  
+    assert result == 'Foo_20250801 result + Foo_20250801 result'
+
